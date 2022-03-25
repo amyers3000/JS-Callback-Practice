3e731ca490c90af86a1cfc815265401a40dc4e0d
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-03-25 10:18:03 -0500

    Completed without bonus

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
+// Reminder: move is a type of function where an element is put in as an argument and then a callback function
+// within that function is invoked using it's key ( there is a return within the funciton that creates an object {to: movecharacter}.... hence the .to(x,y))
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+// It will only run once and direction is not in scope. We also don't want it to run constantly with setInterval, only when direction is changed.
+// Direction added as parameter to solve scope issue
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+// Direction change is in move. We don't want to hardcode the image logic in move but rather use a callback to handleDirectionChange, 
+// that way the function can be invoked when applicable to a situation. In other words move would alert handleDirectionChange that the direction has changed.
+// To solve the scope issue... direction still isn't in scope where we define handleDirectionChange however it is in scope where this function is called upon. Therefore we can pass it in as an argument
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
-
-move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
--- move.js
+++ move.js
@@ -1,3 +1,5 @@
+//  Reminder: move is a type of function where an element is put in as an argument and then a callback function
+// within that function is invoked using it's key ( there is a return within the funciton that creates an object {to: movecharacter}.... hence the .to(x,y))
 function move(element) {
     element.style.position = 'fixed'
 
@@ -6,7 +8,68 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
-    return {
-        to: moveToCoordinates
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+    
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        // Changes character coordinates
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x-=1
+            }
+            if(direction === 'north'){
+                y+=1
+            }
+            if(direction === 'east'){
+                x+=1
+            }
+            if(direction === 'south'){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        // funciton that invokes a call back function every 1 milisecond. Allows logic to repeat
+        setInterval(moveCharacter, 1)
+        // When arrow button is pushed down the direction variable is changed. e.key is part of the event 'keydown' along with 'e.repeat'
+        document.addEventListener('keydown', function(e){
+            // This allows the event to be repeated I believe?
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            // To solve the scope issue... direction still isn't in scope where we define handleDirectionChange however it is in scope where this function is called upon. Therefore we can pass it in as an argument
+            // callback, aka handleDirectionChange, will take new direction and flow through it's logic
+           
+             callback(direction)
+            
+        })
+        // When the button is released the direction goes back to null. No movement.
+        document.addEventListener('keyup', function(e){
+            direction = null
+            // adding callback, aka handle direction change function, to return image to normal
+            // To solve the scope issue... direction still isn't in scope where we define handleDirectionChange however it is in scope where this function is called upon. Therefore we can pass it in as an argument
+           
+            callback(direction)
+            
+        })
     }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
 }
